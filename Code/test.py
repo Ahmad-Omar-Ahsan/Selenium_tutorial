0e8376b3f7b27991f18196e450c2a3bf439d519f
Ahmad Omar Ahsan <gigablaze662@gmail.com>
2020-09-01 22:16:58 +0600

    Completed tutorial 4

--- Code/test.py
+++ Code/test.py
@@ -3,14 +3,14 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+from selenium.webdriver.common.action_chains import ActionChains
 import time
 
 PATH = '/media/omar/Backup/projects/Selenium_tutorial/chromedriver_linux64/chromedriver'
 driver = webdriver.Chrome(PATH)
 
-driver.get("https://techwithtim.net")
-print(driver.title)
+# driver.get("https://techwithtim.net")
+# print(driver.title)
 
 
 # tutorial 2
@@ -56,3 +56,29 @@ try:
 except:
     driver.quit()
 """
+#tutorial 4
+
+driver.get("https://orteil.dashnet.org/cookieclicker/")
+
+driver.implicitly_wait(5)
+
+cookie = driver.find_element_by_id("bigCookie")
+cookie_count = driver.find_element_by_id("cookies")
+items = [driver.find_element_by_id("productPrice" + str(i)) for i in range(1, -1, -1)]
+for item in items:
+    print(item.text)
+
+actions = ActionChains(driver)
+actions.click(cookie)
+
+for i in range(5000):
+    actions.perform()
+    count = int(cookie_count.text.split(" ")[0]) 
+    for item in items:
+        value = int(item.text)
+        if value <= count:
+            upgrade_actions = ActionChains(driver)
+            upgrade_actions.move_to_element(item)
+            upgrade_actions.click()
+            upgrade_actions.perform()
+
