6e02c857ffe1471de912bf77f8aea371410785af
Ahmad Omar Ahsan <gigablaze662@gmail.com>
2020-09-01 15:05:00 +0600

    Completed tutorial 2

--- Code/test.py
+++ Code/test.py
@@ -1,8 +1,28 @@
 from selenium import webdriver 
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+import time
 
 PATH = '/media/omar/Backup/projects/Selenium_tutorial/chromedriver_linux64/chromedriver'
 driver = webdriver.Chrome(PATH)
 
 driver.get("https://techwithtim.net")
 print(driver.title)
+
+search = driver.find_element_by_name("s")
+search.send_keys("test")
+search.send_keys(Keys.RETURN)
+
+try:
+    main = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "main"))
+    )
+    print(main.text)
+except:
+    driver.quit()
+
 driver.quit()
+
