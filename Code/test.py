20b8bc37c60478a26a1ffb5bdcb6507473b87dc6
Ahmad Omar Ahsan <gigablaze662@gmail.com>
2020-09-01 15:05:18 +0600

    Minor changes

--- Code/test.py
+++ Code/test.py
@@ -20,9 +20,14 @@ try:
     main = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.ID, "main"))
     )
-    print(main.text)
-except:
+    
+    articles = main.find_elements_by_tag_name("article")
+    for article in articles:
+        header = article.find_element_by_class_name("entry-summary")
+        print(header.text)
+
+finally:
     driver.quit()
 
-driver.quit()
+
 
